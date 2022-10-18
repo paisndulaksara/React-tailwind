7e6b0b32f4997a91ce3547dee7253234bd2497ad
Pasindu Laksara <pasindulaksara@gmail.com>
2022-10-18 22:38:50 +0530

    MId week tuttoriala

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import { card } from "../assets"
+import styles, {layout} from '../style';
+import Button from './Button';
+
+const Card = () => {
+  
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/>
+      in few easy steps</h2>
+      <p></p>
+    </div>
+  </section>
+}
+
+export default Card
---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonial from "./Testimonial";
+import Hero from "./Hero";
+import FeedbackCard from "./FeedbackCard";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  FeedbackCard,
+  CTA,
+  Stats,
+  Footer,
+  Testimonial,
+  Hero,
+};
